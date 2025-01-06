7b1c141bc6bee4dda49618513e951d96388a3568
SimY4 <alex.simkin@outlook.com>
2025-01-06 15:58:45 +1100

    Added custom keyboard shortcuts for display tiling.

--- macOS.sh
+++ macOS.sh
@@ -5,3 +5,13 @@ xcode-select --install
 echo "Complete the installation of Xcode Command Line Tools before proceeding."
 echo "Press enter to continue..."
 read
+
+defaults write -g NSUserKeyEquivalents -dict-add \
+  Top    '~^\\U2191' \
+  Bottom '~^\\U2193' \
+  Left   '~^\\U2190' \
+  Right  '~^\\U2192' \
+  Center '~^c' \
+  Fill   '~^f' \
+  'Move to Built-in Retina Display' '@~^\\U2190' \
+  'Move to DELL U2723QE' '@~^\\U2192'
