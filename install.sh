d386dc1b658dff18b8f288ea0bac15659a3efacb
SimY4 <alex.simkin@outlook.com>
2025-02-01 19:55:38 +1100

    init .config

--- install.sh
+++ install.sh
@@ -11,4 +11,8 @@ dotfilesdir="$(pwd)"
 # Run the Homebrew Script
 ./brew.sh
 
+# Initializing .config"
+find .config -type d -exec mkdir -p $HOME/{} \;
+find .config -type f -exec ln -sf $dotfilesdir/{} $HOME/{} \;
+
 echo "Installation Complete!"
