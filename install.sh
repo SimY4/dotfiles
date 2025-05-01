0b5ab04cccf53cfc8766af058727283a378da117
SimY4 <alex.simkin@outlook.com>
2025-05-01 18:51:36 +1000

    .ideavimrc config. Zed VIM settings.

--- install.sh
+++ install.sh
@@ -13,6 +13,17 @@ dotfilesdir="$(pwd)"
 
 # Initializing .config"
 find .config -type d -exec mkdir -p $HOME/{} \;
-find .config -type f -exec ln -sf $dotfilesdir/{} $HOME/{} \;
+find .config -type f -exec echo "Linking {}..." \; -exec ln -sf $dotfilesdir/{} $HOME/{} \;
+
+# Define an array of dotfiles
+dotfiles=(
+  ".ideavimrc"
+)
+
+# Loop over the array to link into HOME.
+for dotfile in "${dotfiles[@]}"; do
+  echo "Linking $dotfile..."
+  ln -sf $dotfilesdir/$dotfile $HOME/$dotfile
+done
 
 echo "Installation Complete!"
